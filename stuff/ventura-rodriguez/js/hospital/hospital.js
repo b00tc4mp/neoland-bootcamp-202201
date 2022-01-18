b8964cba2662b839fbf4c973fd3647724821e35f
Ventu Rodríguez <brodriguezserrano54@gmail.com>
2022-01-18 18:38:36 +0100

    add more exmaples of hospital

---
+++ stuff/ventura-rodriguez/js/hospital/hospital.js
@@ -0,0 +1,52 @@
+debugger
+
+var patient1 = {
+    firstName : 'Lucatiel',
+    lastName: 'of Mirrah',
+    age: 27,
+    genre: 'female'
+}
+
+var patient2 = {
+    firstName : 'Artorias',
+    lastName: 'of the Abyss',
+    age: 15267,
+    genre: 'male'
+}
+
+var patient3 = {
+    firstName : 'Sif',
+    lastName: 'of the Abyss',
+    age: 15167,
+    genre: 'male'
+}
+
+
+var list = [
+    patient1,
+    patient2,
+    patient3,
+]
+
+
+var hospital = {
+    patients: list
+}
+
+
+var countMale = 0
+var countFemale = 0
+debugger
+for(var i = 0; i < hospital.patients.length; i++) {
+    debugger
+    if(hospital.patients[i].genre === 'male') {
+        countMale++
+    }
+    else{
+        countFemale++
+    }
+
+}
+
+console.log("total male patients is: " + countMale)
+console.log("total female patients is: " + countFemale)
