a6f4321b6558f9e92fd9be93c8c7d1887bb38eaf
Judith Domènech <judith@MacBook-Pro-de-Judith.local>
2022-01-18 16:21:55 +0100

    cart

---
+++ staff/judith-domenech/web/js/calc/calc.js
@@ -0,0 +1,23 @@
+
+class Calc {
+    
+    constructor (name) {
+        this.name = name
+    }
+
+    add(x, y) {
+        return x + y
+    }
+
+    min(x, y) {
+        return x - y
+    }
+
+    mult(x, y) {
+        return x * y
+    }
+    div(x, y) {
+        return x / y
+    }
+
+}
