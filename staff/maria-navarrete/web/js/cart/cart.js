1b3b4a13e25ce0f89f49ec372936292f7e05ff6c
mariapaulan <mp.navarrete13@gmail.com>
2022-01-18 16:23:14 +0100

    Update maria-lunarillos; add js

---
+++ staff/maria-navarrete/web/js/cart/cart.js
@@ -0,0 +1,24 @@
+debugger
+var socks = {type: 'socks', brand: 'Tommy', qty: 10, price: 20}
+
+var tshirt = {type: 't-shirt', brand: 'Levis', qty: 4, price: 25}
+
+var shoes = {type: 'shoes', brand: 'MK', qty: 2, price: 225}
+
+var cart =[socks, tshirt, shoes]
+
+var total = 0
+
+total = total + cart[0].price * cart[0].qty
+
+total= total + cart[1].price * cart[1].qty
+
+total= total + cart[2].price * cart[2].qty
+
+total = 0
+
+for(var i = 0; i<cart.length; i++){
+    debugger
+    total += cart[i].price + cart[i].qty
+}
+    
