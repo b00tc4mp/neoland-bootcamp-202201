a5886db0cf9f968779184c98127cdc39b04df3b7
Ivan Lopez <ivaan100@gmail.com>
2022-01-18 16:20:43 +0100

    add js files

---
+++ staff/ivan-lopez/web/js/calc/calc.js
@@ -0,0 +1,24 @@
+class Calc {
+
+    constructor(name) {
+
+        this.name = name
+
+    }
+    
+    add(x, y) {
+        return x + y
+    }
+
+    min(x, y) {
+        return x - y
+    }
+
+    mult(x, y) {
+        return x * y
+    }
+
+    div(x, y) {
+        return x / y
+    }
+}
