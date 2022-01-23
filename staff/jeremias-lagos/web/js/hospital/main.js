5e618e4747826d77280f672ddaa5599be31e278d
jere <jere77lagos@gmail.com>
2022-01-23 18:33:09 +0100

    add new files

---
+++ staff/jeremias-lagos/web/js/hospital/main.js
@@ -0,0 +1,79 @@
+var firstNameInput = document.querySelector('.first-name')
+
+firstNameInput.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        var patients = getPatientsByFirstName(firstNameInput.value)
+
+        renderPatients(patients)
+        mechanizeTableClicks()
+    }
+})
+
+var lastNameInput = document.querySelector('.last-name')
+
+lastNameInput.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        var patients = getPatientsByLastName(lastNameInput.value)
+
+        renderPatients(patients)
+        mechanizeTableClicks()
+    }
+})
+
+var genderInput = document.querySelector('.gender')
+
+genderInput.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        var patients = getPatientsByGender(genderInput.value)
+
+        renderPatients(patients)
+        mechanizeTableClicks()
+    }
+})
+
+var yearInput = document.querySelector('.year')
+
+yearInput.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        var patients = getPatientsByYear(parseInt(yearInput.value))
+
+        renderPatients(patients)
+        mechanizeTableClicks()
+    }
+})
+
+var bloodTypeInput = document.querySelector('.blood-type')
+
+bloodTypeInput.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        var patients = getPatientsByBloodType(bloodTypeInput.value)
+
+        renderPatients(patients)
+        mechanizeTableClicks()
+    }
+})
+
+var idInput = document.querySelector('.id')
+
+idInput.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        var patient = getPatientById(idInput.value)
+
+        renderPatients([patient])
+        mechanizeTableClicks()
+    }
+})
+
+var showAllButton = document.querySelector('.show-all')
+
+showAllButton.addEventListener('click', function () {
+    var patients = getShowAll ()
+
+    renderPatients(patients)
+
+    mechanizeTableClicks()
+})
+
+
+
+
