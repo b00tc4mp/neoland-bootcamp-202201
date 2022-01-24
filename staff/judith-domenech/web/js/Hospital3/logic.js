df1ea1c66f0352f85326ba380ad464e0b53e4738
Judith Domènech <judith@MBP-de-Judith.home>
2022-01-24 22:06:29 +0100

    array.prototype

---
+++ staff/judith-domenech/web/js/Hospital3/logic.js
@@ -0,0 +1,88 @@
+function getPatientById(id) {
+    var patient
+
+    for (var i = 0; i < hospital.patients.length; i++)
+        if (hospital.patients[i].id === id) {
+            patient = hospital.patients[i]
+
+            break
+        }
+
+    return patient
+}
+
+function getPatientsByFirstName(firstName) {
+    var patients = []
+    var index = 0
+
+    for (var i = 0; i < hospital.patients.length; i++)
+        if (hospital.patients[i].firstName === firstName) {
+            patients[index] = hospital.patients[i]
+
+            index++
+        }
+
+    return patients
+}
+
+function getPatientsByLastName(lastName) {
+    var patients = []
+    var index = 0
+
+    for (var i = 0; i < hospital.patients.length; i++)
+        if (hospital.patients[i].lastName === lastName) {
+            patients[index] = hospital.patients[i]
+
+            index++
+        }
+
+    return patients
+}
+
+function getPatientsByGender(gender) {
+    var patients = []
+    var index = 0
+
+    for (var i = 0; i < hospital.patients.length; i++)
+        if (hospital.patients[i].gender === gender) {
+            patients[index] = hospital.patients[i]
+
+            index++
+        }
+
+    return patients
+}
+
+function getPatientsByYear(year) {
+    var patients = []
+    var index = 0
+
+    for (var i = 0; i < hospital.patients.length; i++)
+        if (hospital.patients[i].birthDate.getFullYear() === year) {
+            patients[index] = hospital.patients[i]
+
+            index++
+        }
+
+    return patients
+}
+
+function getPatientsByBloodType(bloodType) {
+    var patients = []
+    var index = 0
+
+    for (var i = 0; i < hospital.patients.length; i++)
+        if (hospital.patients[i].bloodType === bloodType) {
+            patients[index] = hospital.patients[i]
+
+            index++
+        }
+
+    return patients
+}
+
+function getShowAll () {
+return hospital.patients
+    
+}
+
---
+++ staff/judith-domenech/web/js/arrays/Prototype method/array.prototype.filter.js
@@ -0,0 +1,6 @@
+const names = ['Peter', 'Laura', 'Mike', 'Jessica', 'Wendy', 'Eustaquia', 'Ester Colero', 'Leandro Gado', 'Ivan atimarte']
+
+
+const result = names.filter(name => name.length > 6);
+
+console.log(result);
