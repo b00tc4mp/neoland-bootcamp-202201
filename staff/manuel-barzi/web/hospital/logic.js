27589d5903d5f8a80d6311b7d1edf4d483dfe8ee
manuelbarzi <manuelbarzi@gmail.com>
2022-01-24 13:03:02 +0100

    add logic for getting patients by year

--- staff/manuel-barzi/web/hospital/logic.js
+++ staff/manuel-barzi/web/hospital/logic.js
@@ -7,3 +7,5 @@ const getPatientsByLastName = lastName => hospital.patients.filter(patient => pa
 const getPatientsByGender = gender => hospital.patients.filter(patient => patient.gender === gender)
 
 const getPatientsByBloodType = bloodType => hospital.patients.filter(patient => patient.bloodType === bloodType)
+
+const getPatientsByYear = year => hospital.patients.filter(patient => patient.birthDate.getFullYear() === year)
