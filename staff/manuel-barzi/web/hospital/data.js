2e6ab2609c0bcec98ba6a4104f7cc5cff2f2b082
manuelbarzi <manuelbarzi@gmail.com>
2022-01-24 10:32:25 +0100

    add new data generation with randoms

--- staff/manuel-barzi/web/hospital/data.js
+++ staff/manuel-barzi/web/hospital/data.js
@@ -1,4 +1,4 @@
-function Person(id, firstName, lastName, gender, birthDate, bloodType, city, country) {
+function Person(id, firstName, lastName, gender, birthDate, bloodType, city, country, email, phone) {
     this.id = id
     this.firstName = firstName
     this.lastName = lastName
@@ -7,358 +7,51 @@ function Person(id, firstName, lastName, gender, birthDate, bloodType, city, cou
     this.bloodType = bloodType
     this.city = city
     this.country = country
+    this.email = email
+    this.phone = phone
 }
 
-var ele = new Person(
-    generateId(),
-    'Ele',
-    'Fante',
-    'female',
-    newDate(1994, 1, 15),
-    'A+',
-    'New York City',
-    'United States of America'
-)
 
-var suzana = new Person(
-    generateId(),
-    'Suzana',
-    'Horia',
-    'female',
-    newDate(1994, 11, 20),
-    'A-',
-    'New York City',
-    'United States of America'
-)
+var firstNames = ['Pepito', 'John', 'Manuel', 'Matias', 'Paula', 'María', 'Judith', 'Alba', 'Alejandro', 'Gio', 'Joel', 'Nicolás', 'Jeremías', 'Ventura', 'Iván', 'Susan', 'Francesc', 'Zoraida', 'Victor', 'Yastá', 'Noseyo', 'Elba', 'Elber', 'Aitor', 'Cindy', 'Ana', 'Bat']
 
-var ti = new Person(
-    generateId(),
-    'Ti',
-    'Greton',
-    'male',
-    newDate(1992, 06, 13),
-    'AB+',
-    'New York City',
-    'United States of America'
-)
+var lastNames = ['Grillo', 'Lennon', 'Barzi', 'Prats', 'Mandioca', 'Tigre', 'Esponja', 'Lechuga', 'Hernández', 'Gonzáles', 'Fumeta', 'Ventura', 'Rodríguez', 'García', 'Lorca', 'Pirulo', 'Campo', 'López', 'Canal', 'Zanahoria', 'Espineta', 'Patata', 'Surero', 'Nero', 'Capullín', 'Alelí']
 
-var vi = new Person(
-    generateId(),
-    'Vi',
-    'Bora',
-    'female',
-    newDate(1990, 6, 18),
-    'AB-',
-    'New York City',
-    'United States of America'
-)
+var genders = ['female', 'male', 'other']
 
-var luisca = new Person(
-    generateId(),
-    'Luisca',
-    'Chondo',
-    'male',
-    newDate(2001, 1, 15),
-    'B+',
-    'New York City',
-    'United States of America'
-)
+var bloodTypes = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']
 
-var pepito = new Person(
-    generateId(),
-    'Pepito',
-    'Grillo',
-    'male',
-    newDate(2000, 12, 12),
-    'B-',
-    'New York City',
-    'United States of America'
-)
+var cities = ['Platano', 'Naranja', 'Kiwi', 'Arandonos', 'Manzana', 'Pera', 'Kaki', 'Uva', 'Piña', 'Sandía', 'Melón', 'Mango']
+var countries = ['Rojo', 'Verde', 'Amarillo', 'Azul', 'Violeta', 'Rosa', 'Carmín', 'Turquesa', 'Duquesa', 'Morado', 'Sótano']
 
-var elba = new Person(
-    generateId(),
-    'Elba',
-    'Surero',
-    'female',
-    newDate(1986, 3, 10),
-    'O+',
-    'New York City',
-    'United States of America'
-)
+var people = []
 
-var elber = new Person(
-    generateId(),
-    'Elber',
-    'Galarga',
-    'male',
-    newDate(1967, 9, 21),
-    'O-',
-    'New York City',
-    'United States of America'
-)
+for (var i = 0; i < 1000; i++) {
+    var id = generateId()
 
-var ana = new Person(
-    generateId(),
-    'Ana',
-    'Conda',
-    'female',
-    newDate(1986, 10, 03),
-    'A+',
-    'New York City',
-    'United States of America'
-)
+    var firstName = randomElement(firstNames)
+    var lastName = randomElement(lastNames)
 
-var bob = new Person(
-    generateId(),
-    'Bob',
-    'Esponja',
-    'male',
-    newDate(1995, 05, 24),
-    'O+',
-    'New York City',
-    'United States of America'
-)
+    var gender = randomElement(genders)
+    
+    var bloodType = randomElement(bloodTypes)
+    
+    var day = randomInteger(1, 31)
+    var month = randomInteger(1, 12)
+    var year = randomInteger(1900, 2022)
+    
+    var birthDate = newDate(year, month, day)
+    
+    var city = randomElement(cities) + ' City'
+    var country = randomElement(countries)
+    
+    var email = firstName.toLowerCase() + '.' + lastName.toLowerCase() + '@mail.com'
+    var phone = randomInteger(600000000, 790000000)
 
-var elena = new Person(
-    generateId(),
-    'Elena',
-    'De Troya',
-    'female',
-    newDate(1992, 01, 11),
-    'B+',
-    'New York City',
-    'United States of America'
-)
+    var person = new Person(id, firstName, lastName, gender, birthDate, bloodType, city, country, email, phone)
 
-var armando = new Person(
-    generateId(),
-    'Armando',
-    'Guerra',
-    'male',
-    newDate(2010, 8, 8),
-    'B-',
-    'New York City',
-    'United States of America'
-)
-
-var mort = new Person(
-    generateId(),
-    'Mort',
-    'Adela',
-    'female',
-    newDate(1890, 8, 30),
-    'B+',
-    'New York City',
-    'United States of America'
-)
-
-var lola = new Person(
-    generateId(),
-    'Lola',
-    'Mento',
-    'female',
-    newDate(2000, 12, 12),
-    'AB-',
-    'New York City',
-    'United States of America'
-)
-
-var andy = new Person(
-    generateId(),
-    'Andy',
-    'Garcia',
-    'male',
-    newDate(1986, 3, 10),
-    'AB-',
-    'New York City',
-    'United States of America'
-)
-
-var aitor = new Person(
-    generateId(),
-    'Aitor',
-    'Tilla',
-    'male',
-    newDate(1986, 3, 10),
-    'AB+',
-    'New York City',
-    'United States of America'
-)
-
-var pere = new Person(
-    generateId(),
-    'Pere',
-    'Gil',
-    'male',
-    newDate(1986, 4, 20),
-    'O-',
-    'New York City',
-    'United States of America'
-)
-
-var juanca = new Person(
-    generateId(),
-    'Juanca',
-    'Daver',
-    'male',
-    newDate(2001, 5, 16),
-    'O-',
-    'New York City',
-    'United States of America'
-)
-
-var ador = new Person(
-    generateId(),
-    'Ador',
-    'Nandoarboles',
-    'male',
-    newDate(1997, 10, 25),
-    'O+',
-    'New York City',
-    'United States of America'
-)
-
-var son = new Person(
-    generateId(),
-    'Son',
-    'Goku',
-    'male',
-    newDate(1986, 8, 22),
-    'A+',
-    'New York City',
-    'United States of America'
-)
-
-var bat = new Person(
-    generateId(),
-    'Bat',
-    'Man',
-    'male',
-    newDate(1986, 3, 10),
-    'O+',
-    'New York City',
-    'United States of America'
-)
-
-var spider = new Person(
-    generateId(),
-    'Spider',
-    'Man',
-    'male',
-    newDate(1986, 4, 20),
-    'O-',
-    'New York City',
-    'United States of America'
-)
-
-var electra = new Person(
-    generateId(),
-    'Electra',
-    'Cuta',
-    'female',
-    newDate(2001, 5, 16),
-    'B-',
-    'New York City',
-    'United States of America'
-)
-
-var andy2 = new Person(
-    generateId(),
-    'Andy',
-    'Garcia',
-    'male',
-    newDate(1997, 10, 25),
-    'B+',
-    'New York City',
-    'United States of America'
-)
-
-var cho = new Person(
-    generateId(),
-    'Cho',
-    'Lizo',
-    'male',
-    newDate(1986, 8, 22),
-    'AB+',
-    'New York City',
-    'United States of America'
-)
-
-var e = new Person(
-    generateId(),
-    'E',
-    'Lizo',
-    'male',
-    newDate(1986, 3, 10),
-    'B-',
-    'New York City',
-    'United States of America'
-)
-
-var e2 = new Person(
-    generateId(),
-    'E',
-    'Lefante',
-    'female',
-    newDate(1986, 4, 20),
-    'A-',
-    'New York City',
-    'United States of America'
-)
-
-var elba2 = new Person(
-    generateId(),
-    'Elba',
-    'Ticano',
-    'female',
-    newDate(1992, 01, 11),
-    'O-',
-    'New York City',
-    'United States of America'
-)
-
-var elba3 = new Person(
-    generateId(),
-    'Elba',
-    'Lazo',
-    'female',
-    newDate(2001, 5, 16),
-    'A-',
-    'New York City',
-    'United States of America'
-)
-
-var people = [
-    ele,
-    suzana,
-    ti,
-    vi,
-    luisca,
-    pepito,
-    elba,
-    elber,
-    ana,
-    bob,
-    elena,
-    armando,
-    mort,
-    lola,
-    andy,
-    aitor,
-    pere,
-    juanca,
-    ador,
-    son,
-    bat,
-    spider,
-    electra,
-    andy2,
-    cho,
-    e,
-    e2,
-    elba2,
-    elba3
-]
+    people[i] = person
+}
+    
 
 var hospital = {
     patients: people
--- staff/manuel-barzi/web/hospital/helpers.js
+++ staff/manuel-barzi/web/hospital/helpers.js
@@ -1,7 +1,17 @@
 function generateId() {
-    return parseInt(Math.random() * 1000000000).toString()
+    return randomInteger(100000000, 999999999).toString() + randomElement('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 }
 
 function newDate(year, month, day) {
     return new Date(year, month - 1, day)
+}
+
+function randomInteger(min, max) {
+    return min + Math.round((max - min) * Math.random())
+}
+
+function randomElement(iterable) {
+    var index = randomInteger(0, iterable.length - 1)
+
+    return iterable[index]
 }
