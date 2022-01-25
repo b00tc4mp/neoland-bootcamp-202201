13cf8e35de80b788457fd4cfb2c936bb15a8ad6e
Alba Casas <albacasasarzua1b@gmail.com>
2022-01-25 21:41:05 +0100

    Solve indexOf

--- staff/alba-casas/web/js/strings/customFunctions/indexOf.js
+++ staff/alba-casas/web/js/strings/customFunctions/indexOf.js
@@ -3,11 +3,24 @@ const text = "hello world";
 const indexOf = (string, searchValue) => {
   for (let i = 0; i < string.length; i++) {
     const char = string[i];
+    const nextChar = string[i + 1];
+    if (char === searchValue[0] && nextChar === searchValue[1]) return i;
+  }
+  return -1;
+};
+
+const result = indexOf(text, "ld");
+
+console.log(result);
+
+const indexOf2 = (string, searchValue) => {
+  for (let i = 0; i < string.length; i++) {
+    const char = string[i];
     if (char === searchValue) return i;
   }
   return -1;
 };
 
-const result = indexOf(text, "w");
+const result2 = indexOf2(text, "l");
 
-console.log(result);
+console.log(result2);
