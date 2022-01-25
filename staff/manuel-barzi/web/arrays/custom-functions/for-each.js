fe64b690f5f8976e5e9b59f159b9b4de99ffd117
manuelbarzi <manuelbarzi@gmail.com>
2022-01-25 09:32:10 +0100

    add some and every files

--- staff/manuel-barzi/web/arrays/custom-functions/for-each.js
+++ staff/manuel-barzi/web/arrays/custom-functions/for-each.js
@@ -1,5 +1,13 @@
 const names = ['Peter', 'Laura', 'Mike', 'Jessica', 'Wendy']
 
+// function forEach(array, callback) {
+//     for (let i = 0; i < array.length; i++) {
+//         const element = array[i]
+
+//         callback(element)
+//     }
+// }
+
 function forEach(array, callback) {
     for (let i = 0; i < array.length; i++)
         callback(array[i])
