bc1a63a5f80fbc9ac59498cb0134d035906e6406
Alba Casas <albacasasarzua1b@gmail.com>
2022-01-26 11:17:13 +0100

    Solve reduce

--- staff/alba-casas/web/js/arrays/customFuctions/reduce.js
+++ staff/alba-casas/web/js/arrays/customFuctions/reduce.js
@@ -1,1 +1,15 @@
-const array1 = [1, 2, 3, 4];
+const numbers = [1, 2, 3, 4];
+
+let total = 0;
+numbers.forEach((num) => (total += num));
+
+console.log(total);
+
+const numbers2 = [2, 5, 7];
+
+let sum = 0;
+for (let i = 0; i < numbers2.length; i++) {
+  sum += numbers2[i];
+}
+
+console.log(sum);
--- staff/alba-casas/web/js/arrays/prototypeMethods/array.prototype.reduce.js
+++ staff/alba-casas/web/js/arrays/prototypeMethods/array.prototype.reduce.js
@@ -1,4 +1,4 @@
-const array1 = [1, 2, 3, 4];
+const array = [1, 2, 3, 4];
 const reducer = (previousValue, currentValue) => previousValue + currentValue;
 
-console.log(array1.reduce(reducer));
+console.log(array.reduce(reducer));
