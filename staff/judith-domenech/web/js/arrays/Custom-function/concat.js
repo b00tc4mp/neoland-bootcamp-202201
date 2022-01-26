6f7dea72a2c17a8f03076edc79f3ad717cae3d55
Judith Domènech <judith@MBP-de-Judith.home>
2022-01-27 00:30:46 +0100

    concat

--- staff/judith-domenech/web/js/arrays/Custom-function/concat.js
+++ staff/judith-domenech/web/js/arrays/Custom-function/concat.js
@@ -1,17 +1,51 @@
 const nombre1 = ['Judith', 'Paca', 'Isi'];
 const nombre2 = ['Eustaquia', 'Esther', 'Colero'];
 
-function concat (array, )
+function concat (array, array2){
+   const newArray =[]
+   let index = 0
+   
+    for(let i = 0; i < array.length; i++)
+     newArray[index] = array + ', ' + array2; {  
+         index++
+    }  return newArray
+}
+
+console.log(concat(nombre1, nombre2))
+
+//[ 'Judith,Paca,Isi, Eustaquia,Esther,Colero' ]
 
 
 
 
+/* const nombre1 = ['Judith', 'Paca', 'Isi'];
+const nombre2 = ['Eustaquia', 'Esther', 'Colero'];
+
+function concat (array, array2){
+    newArray=[]
+    for(let i = 0; i < array.length; i++)
+     newArray[i] = array[i] + ' ' + array2[i]; {
+         return newArray
+     }
+}
+
+console.log(concat(nombre1, nombre2))
+ */
 
 
 
+/* const nombre1 = ['Judith', 'Paca', 'Isi'];
+const nombre2 = ['Eustaquia', 'Esther', 'Colero'];
 
+function concat (array, array2){
+   const newArray=[]
+   
+    for(let i = 0; i < array.length; i++)
+     newArray[i] = array + ', ' + array2; {  
+    } 
 
+    return newArray
+}
 
-crear un nuevo array, 
-recorreer los strings y que el valor de cada posicion se añada al string creado
-imprimir nuevo array creado
+console.log(concat(nombre1, nombre2))
+ */
--- staff/judith-domenech/web/js/arrays/Custom-function/indexOf.js
+++ staff/judith-domenech/web/js/arrays/Custom-function/indexOf.js
@@ -1,7 +1,7 @@
 const numbers = [2, 9, 3, 4, 5, 2, 5, 8];
 
 function indexOf (array, number, index) {
-    debugger
+    
     for(let i = index; i < array.length; i++ ){
 
         if(array[i] === number) {
