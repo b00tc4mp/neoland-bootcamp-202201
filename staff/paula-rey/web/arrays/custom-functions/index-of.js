ecad991574882526ae115753461c8acf429c87c2
Paula Rey <preynava@gmail.com>
2022-01-26 18:43:16 +0100

    add arrays

---
+++ staff/paula-rey/web/arrays/custom-functions/index-of.js
@@ -0,0 +1,25 @@
+const numbers = [2, 5, 7, 9 ,8,7,5]
+
+function indexOf(array, searchElement) {
+
+    for(let i = 0; i < array.length; i++) {
+        const element = array[i]
+        if(element === searchElement) {
+            return i
+        }
+    }
+}
+
+const indexOf = (array, searchElement) => {
+    for(let i = 0; i < array.length; i++) if(array[i] === searchElement) return i
+}
+
+const res = indexOf(numbers, 7) 
+// expected output: 2
+
+console.log(res)
+
+// Me voy a buscar al array,
+// recorro el array uno en uno,
+// comparo si mi searchElement es igual al elemento del array que estoy mirando en cada iteración
+// devuelvo la posición del elemento encontrado
