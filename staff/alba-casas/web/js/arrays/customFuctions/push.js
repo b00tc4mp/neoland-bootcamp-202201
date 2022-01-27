1c3ff335557ffa38230d741d7c20a03ee5d32c04
Alba Casas <albacasasarzua1b@gmail.com>
2022-01-27 14:48:56 +0100

    Solve push and concat

--- staff/alba-casas/web/js/arrays/customFuctions/push.js
+++ staff/alba-casas/web/js/arrays/customFuctions/push.js
@@ -1,8 +1,21 @@
 const animals = ["pigs", "goats", "sheep"];
+const animals2 = ["cow", "rat"];
 
 function push(array, value) {
   array[array.length] = value;
+  return array.length;
+}
+
+function push2(array, ...args) {
+  for (let i = 1; i < args.length; i++) {
+    array[array.length] = args[i];
+  }
+  return array.length;
+}
+
+function push3(array, array2) {
+  array = [...array, ...array2];
   return array;
 }
 
-console.log(push(animals, ["cow", "rat"]));
+console.log(push3(animals, animals2));
--- staff/alba-casas/web/js/arrays/prototypeMethods/array.prototype.push.js
+++ staff/alba-casas/web/js/arrays/prototypeMethods/array.prototype.push.js
@@ -3,3 +3,5 @@ const animals = ["pigs", "goats", "sheep"];
 const count = animals.push("cows");
 
 console.log(animals, count);
+
+console.log(animals.push("frog"));
