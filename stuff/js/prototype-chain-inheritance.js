57c3e8b9fa1601c1d46c18b1eccde1596ff1ea8a
manuelbarzi <manuelbarzi@gmail.com>
2022-01-28 11:24:18 +0100

    update prototype demo

--- stuff/js/prototype-chain-inheritance.js
+++ stuff/js/prototype-chain-inheritance.js
@@ -1,30 +1,47 @@
+/**
+ * Abstraction of a Person
+ * 
+ * @param {string} name The person name.
+ */
 function Person(name) {
     this.name = name
 }
 
-Person.prototype.salute = function(to) {
+Person.prototype.salute = function (to) {
     return `${this.name}: Hello, ${to.name}!`
 }
 
-
+/**
+ * Abstraction of a Woman
+ * 
+ * @param {string} name The woman name.
+ */
 function Woman(name) {
     this.name = name
 }
 
-Woman.prototype = new Person()
+//Woman.prototype = new Person()
+Woman.prototype = Object.create(Person.prototype)
+Woman.prototype.constructor = Woman
 
-Woman.prototype.giveBirth = function() {
+Woman.prototype.giveBirth = function () {
     return '👶🏻'
 }
 
-
+/**
+ * Abstraction of Man.
+ * 
+ * @param {string} name The name of the man.
+ */
 function Man(name) {
     this.name = name
 }
 
-Man.prototype = new Person()
+// Man.prototype = new Person()
+Man.prototype = Object.create(Person.prototype)
+Man.prototype.constructor = Man
 
-Man.prototype.giveSperm = function() {
+Man.prototype.giveSperm = function () {
     return '💦'
 }
 
@@ -35,10 +52,45 @@ var peter = new Man('Peter')
 console.log(peter.salute(wendy))
 console.log(wendy.salute(peter))
 
+console.log(peter instanceof Object) // true
 console.log(peter instanceof Person) // true
 console.log(peter instanceof Man) // true
 console.log(peter instanceof Woman) // false
 
+console.log(wendy instanceof Object) // true
 console.log(wendy instanceof Person) // true
-console.log(wendy instanceof Man) // false
 console.log(wendy instanceof Woman) // true
+console.log(wendy instanceof Man) // false
+
+var mati = new Man('Maty')
+var mPau = new Woman('María Paula')
+var gio = new Man('Giovanni')
+var pau = new Woman('Paula')
+var alba = new Woman('Alba')
+var jud = new Woman('Judith')
+var alex = new Man('Alejandro')
+var joe = new Man('Joel')
+var jere = new Man('Jeremías')
+var ivan = new Man('Iván')
+var ventu = new Man('Ventu')
+var manu = new Man('Manuel')
+
+var students = [mati, mPau, gio, pau, alba, jud, alex, joe, jere, ivan, ventu, manu]
+
+var womenCount = students.reduce((accum, student) => {
+    if (student instanceof Woman)
+        return accum + 1
+
+    return accum
+}, 0)
+
+console.log(womenCount)
+
+var menCount = students.reduce((accum, student) => {
+    if (student instanceof Man)
+        return accum + 1
+
+    return accum
+}, 0)
+
+console.log(menCount)
