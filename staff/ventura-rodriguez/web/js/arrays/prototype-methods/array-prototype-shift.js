62146a6b5119e5f5ed5e6cd8ab458c9d79dbadf5
Ventu Rodríguez <brodriguezserrano54@gmail.com>
2022-01-30 12:49:18 +0100

    add game

---
+++ staff/ventura-rodriguez/web/js/arrays/prototype-methods/array-prototype-shift.js
@@ -0,0 +1,9 @@
+const arr = ['hello', 'hola', 'salute', 'hallo']
+
+const result = arr.shift()
+
+console.log(result) // Expected output: 'hello'
+
+console.log(arr.length) // Expected output: 3
+
+console.log(arr[0]) // Expected output: 'hola'
