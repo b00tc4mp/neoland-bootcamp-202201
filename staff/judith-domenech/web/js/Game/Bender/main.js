488248b0baae97ad3e525b2c427164aad9d7eb20
Judith Domènech <judith@MacBook-Pro-de-Judith.local>
2022-01-31 09:42:54 +0100

    Game

--- staff/judith-domenech/web/js/Game/Bender/main.js
+++ staff/judith-domenech/web/js/Game/Bender/main.js
@@ -1,24 +1,42 @@
-const face = document.createElement('div')
-face.classList.add('face')
+const bender = new Roboloko('lightgray', 125, 150)
 
-const eyes = document.createElement('div')
-eye.classList.add('eyes')
+bender.moveRight(200)
+bender.moveDown(200)
 
-const eye = document.createElement('div')
-eye.classList.add('eye')
+bender.container.addEventListener('click', () => {
+    alert('I want to drink something!!')
 
+   /*  tomato.toggleTooth() */
+})
 
+const beer = new Beer('brown', 75, 125)
 
-
-
-
-
-
-
-
-face.append(eye)
-face.append(mouth)
+beer.moveRight(350)
+beer.moveDown(225)
 
 
-document.body.append(face)
-document.add
+document.body.append(bender.container)
+document.body.append(beer.container)
+
+
+document.addEventListener('keydown', event => {
+    const { key } = event
+
+    if (key === 'ArrowUp')
+        bender.moveUp(10)
+    else if (key === 'ArrowDown')
+        bender.moveDown(10)
+    else if (key === 'ArrowLeft')
+        bender.moveLeft(10)
+    else if (key === 'ArrowRight')
+        bender.moveRight(10)
+
+    if (key === 'w')
+        beer.moveUp(10)
+    else if (key === 's')
+        beer.moveDown(10)
+    else if (key === 'a')
+        beer.moveLeft(10)
+    else if (key === 'd')
+        beer.moveRight(10)
+})
