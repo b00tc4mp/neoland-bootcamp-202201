39b3da85de335b04f3aa7e9d3d13da5a620e7b58
Aleperaza <alejandroperaza286@gmail.com>
2022-02-01 15:32:51 +0000

    demo

--- staff/alejandro-peraza/web/demo.app/logic.js
+++ staff/alejandro-peraza/web/demo.app/logic.js
@@ -1,15 +1,26 @@
+const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const BLANK_REGEX = /^\s+$/
+const SPACE_REGEX = /\s/
+const SPACES_AROUND_REGEX = /^\s[aA-zZ]\s?[aA-zZ]|[aA-zZ]\s?[aA-zZ]\s$/
+
 function authenticateUser(email, password) {
+
+    validateEmail(email)
+    validatePassword(password)
+
 const user = users.find(user => user.email === email && user.password === password)
 
     if (user) {
         return user.id
     }
     
-    return null
+    throw new Error('wrong credentials')
 
 }
 
 function retrieveUser(id) {
+    validateId(id)
+    
     const user = users.find(user => user.id === id)
     
     if (user) {
@@ -19,16 +30,42 @@ function retrieveUser(id) {
     return null
 }
 
-function registerUser (name, surname, email, password) {
-    const user = new User(name, surname, email, password)
+function registerUser(name, surname, email, password) {
+    validateName(name)
+    validateSurname(surname)
+    validateEmail(email)
+    validatePassword(password)
 
+    
+    let user = users.find(user => user.email === email)
+
+    if (user) throw new Error('user already exist')
+
+    user = new User(name, surname, email, password)
+    
     user.push(user)
 }
 
-function updateUser(id, name, surname, email, password) {
+function updateUser(id, name, surname, email) {
+    validateId(id)
+    validateName(Name)
+    validateSurname(surname)
+    validateEmail(email)
+    
+    
+    const user = user.find(user => user.id === id)
+
+    if (!user) throw Error('user not found')
+    
+    if (user.password !== currPassword) throw new Error('wrong credentials')
+    
+    if (password !== rePassword) throw new Error('password and password retyped do not match')
+    
+    user.password = password
 
 }
 
 function upregisterUser(id, password) {
-    
-}
+    validateId(id)
+    validatePassword(password)    
+}           
--- staff/alejandro-peraza/web/demo.app/main.js
+++ staff/alejandro-peraza/web/demo.app/main.js
@@ -3,16 +3,13 @@ const loginView = document.querySelector('.login')
 const registerView = document.querySelector('.register')
 const homeView = document.querySelector('.home')
 const profileView = document.querySelector('profile')
+const updatePasswordView = document.querySelector('.update-password')
+const deleteAccountView = document.querySelector('.delete-account')
 
 const landingLoginLink = landingView.querySelector('.landing__login')
 const landingRegisterLink = landingView.querySelector('.landing__register')
-const homeLogoutButton = homeView.querySelector('.home__logout-button')
-const profileUnregisterButton = profileView.querySelector('.profile__unregister-button')
 
-
-// landingLoginLink.addEventListener('click', event => {
-//     event.preventDefault()
-// })
+let userId
 
 landingLoginLink.onclick = event => {
     event.preventDefault()
@@ -59,18 +56,26 @@ loginForm.onsubmit = event => {
     const email = emailInput.value
     const password = passwordInput.value
 
-    const id = authenticateUser(email, password)
+ 
 
-    if (id) {
-        const user = retrieveUser(id)
+    try {
+        userId = authenticateUser(email, password)
 
-        const homeWelcome = homeView.querySelector('.home__welcome')
-        homeWelcome.innerText = `Hello, ${user.name}!`
+        const user = retrieveUser(userid)
+
+        emailInput.value = ''
+        passwordInput.value = ''
+
+        const homeUser = homeView.querySelector('.home__user')
+        homeuser.innerText = user.name
 
         loginView.classList.add('off')
 
         homeView.classList.remove('off')
-    } else alert('wrong credentials')
+    } catch (error) {
+        alert(error.message)
+        passwordInput.value = ''
+    }
 }
 
 const registerForm = registerView.querySelector('.register__form')
@@ -78,36 +83,159 @@ const registerForm = registerView.querySelector('.register__form')
 registerForm.onsubmit = event => {
     event.preventDefault()
 
-    const nameInput = registerForm.querySelector('.register__name')
-    const surnameInput = registerForm.querySelector('.register__surname')
-    const emailInput = registerForm.querySelector('.register__email')
-    const passwordInput = registerForm.querySelector('.register__password')
+    const nameInput = registerForm.querySelector('.register__name-input')
+    const surnameInput = registerForm.querySelector('.register__surname-input')
+    const emailInput = registerForm.querySelector('.register__email-input')
+    const passwordInput = registerForm.querySelector('.register__password-input')
 
     const name = nameInput.value
     const surname = surnameInput.value
     const email = emailInput.value
     const password = passwordInput.value
 
+    try {
     registerUser(name, surname, email, password)
+    
+    name = nameInput.value = ''
+    surname = surnameInput.value = ''
+    email = emailInput.value = ''
+    password = passwordInput.value = ''
+
+    
 
     registerView.classList.add('off')
 
     loginView.classList.remove('off')
+    } catch (error){
+        alert(error.message)
+    }
 }
+const homeLogoutButton = homeView.querySelector('.home_logout-button')
 
-homeLogoutButton.onclick = event => {
-    event.preventDefault()
-
+homeLogoutButton.onclick = () => {
     homeView.classList.add('off')
+    profileView.classList.add('off')
+    updatePasswordView.classList.add('off')
+    deleteAccountView.classList.add('off')
+    
     landingView.classList.remove('off')
 
 }
 
+const homeProfileLink = homeView.querySelector('.home__profile-link')
 
 
-homeView.onclick = event => {
+homeProfileLink.onclick = event => {
+    event.preventDefault()
+
+    const profileNameInput = profileView.querySelector('.profile__name-input')
+    const profileSurnameInput = profileView.querySelector('.profile__surname-input')
+    const profileEmailInput = profileView.querySelector('.profile__email-input')
+
+    const user = retrieveUser(userId)
+
+    profileNameInput.value = user.name
+    profileSurnameInput.value = user.surname
+    profileEmailInput.value = user.email
+
+    updatePasswordView.classList.add('off')
+    deleteAccountView.classList.add('off')
+
+    profileView.classList.remove('off')
+    
+}
+const homeHomeLink = homeView.querySelector('.home__home-link')
+
+homeHomeLink.onclick = event => {
     event.preventDefault()
 
     profileView.classList.add('off')
-    homeView.classList.remove('off')
+}
+
+const profileUpdatePasswordLink = profileView.querySelector('.profile__update-password-link')
+
+profileUpdatePasswordLink.onclick = event => {
+    event.preventDefault()
+
+    profileView.classList.add('off')
+
+    updatePasswordView.classList.remove('off')
+}
+
+const updatePasswordBackLink = updatePasswordView.querySelector('.update-password__back-link')
+
+updatePasswordBackLink.onclick = event => {
+    event.preventDefault()
+
+    updatePasswordView.classList.add('off')
+
+    profileView.classList.remove('off')
+}
+
+const profileDeleteAccountLink = profileView.querySelector('.profile__delete-account-link')
+
+profileDeleteAccountLink.onclick = event => {
+    event.preventDefault()
+
+    profileView.classList.add('off')
+
+    deleteAccountView.classList.remove('off')
+}
+
+const deleteAccountBackLink = deleteAccountView.querySelector('.delete-account__back-link')
+
+deleteAccountBackLink.onclick = event => {
+    event.preventDefault()
+
+    deleteAccountView.classList.add('off')
+    
+    profileView.classList.remove('off')
+}
+
+const profileForm = profileView.querySelector('.profile__form')
+
+profileForm.onsubmit = event => {
+    event.preventDefault()
+
+    const nameInput = profileForm.querySelector('.profile__name-input')
+    const surnameInput = profileForm.querySelector('.profile__surname-input')
+    const emailInput = profileForm.querySelector('.profile__email-input')
+
+    const name = nameInput.value
+    const surname = surnameInput.value
+    const email = emailInput.value
+
+    try {
+        updateUser(userId, name, surname, email)
+
+        alert('user updated')
+    } catch(error) {
+        alert(error.message)
+    }
+}
+
+const updatePasswordForm = updatePasswordView.querySelector('.update-password__form')
+
+updatePasswordForm.onsubmit = event => {
+    event.preventDefault()
+
+    const currPasswordInput = updatePasswordForm.querySelector('.update-password__curr-password-input')
+    const passwordInput = updatePasswordForm.querySelector('.update-password__password-input')
+    const rePasswordInput = updatePasswordForm.querySelector('.update-password__re-password-input')
+
+    const currPassword = currPasswordInput.value
+    const password = passwordInput.value
+    const rePassword = rePasswordInput.value
+
+    try {
+        updateUserPassword(userId, currPassword, password, rePassword)
+
+        currPasswordInput.value = ''
+        passwordInput.value = ''
+        rePasswordInput.value = ''
+
+        alert('password updated')
+    } catch(error) {
+        alert(error.message)
+    }
 }
