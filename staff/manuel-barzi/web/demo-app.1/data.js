007b8db7c5cb161d27ea9f441224b3f7cf571ad8
manuelbarzi <manuelbarzi@gmail.com>
2022-02-01 13:19:32 +0100

    boost demo-app

--- staff/manuel-barzi/web/demo-app.1/data.js
+++ staff/manuel-barzi/web/demo-app.1/data.js
@@ -3,7 +3,7 @@ let userCount = 0
 function User(name, surname, email, password) {
     userCount++
 
-    this.id = `${userCount}`
+    this.id = userCount
     this.name = name
     this.surname = surname
     this.email = email
