9c66e8221be717646ac6176a700da74f9f11d330
Judith Domènech <judith@MacBook-Pro-de-Judith.local>
2022-02-02 12:49:26 +0100

    demo app

---
+++ staff/judith-domenech/web/js/arrays/Prototype-method/array.prototype.splice.js
@@ -0,0 +1,16 @@
+const months = ['Jan', 'March', 'April', 'June'];
+months.splice(1, 0, 'Feb');
+// inserts at index 1
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "June"]
+
+months.splice(4, 1, 'May');
+// replaces 1 element at index 4
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "May"]
+
+
+
+
+
+/* El método splice() cambia el contenido de un array eliminando elementos existentes y/o agregando nuevos elementos. */
