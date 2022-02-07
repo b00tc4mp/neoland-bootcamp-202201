3d1994ed82cbc90e5aea92f156652472ec565df6
Aleperaza <alejandroperaza286@gmail.com>
2022-02-07 00:22:48 +0000

    refixing demo app

--- staff/alejandro-peraza/web/demo.app/components/search.js
+++ staff/alejandro-peraza/web/demo.app/components/search.js
@@ -12,7 +12,7 @@ searchForm.onsubmit = event => {
             const resultsList = searchView.querySelector('.search__results-list')
             const detailView = searchView.querySelector('.detail')
             
-            resultsListView.innerHTML = ''
+            resultsList.innerHTML = ''
 
             vehicles.forEach(vehicle => {
                 const listItem = document.createElement('li')
@@ -34,6 +34,7 @@ searchForm.onsubmit = event => {
                 resultsList.append(listItem)
             
 
+            resultsList.classList.remove('off')
             listItem.addEventListener('click', event => { // listItem.onclick = event => { .... }
                 retrieveVehicle(vehicle.id)
                     .then(({ name, image, description, price, url }) => {
@@ -67,7 +68,7 @@ searchForm.onsubmit = event => {
 
                         detailView.append(itemTitle, itemImage, itemDescription, itemPrice, itemLink, backLink)
 
-                        resultsListView.classList.add('off')
+                        resultsListView.classList.remove('off')
                         detailView.classList.remove('off')
                     })
             })
