6df050fae23df53e3f395f06ab4333571995dc51
Aleperaza <alejandroperaza286@gmail.com>
2022-02-09 09:37:26 +0000

    zen app

---
+++ staff/alejandro-peraza/web/demo-app/src/components/Home1.js
@@ -0,0 +1,41 @@
+import './Home.css'
+import Profile from './Profile'
+import UpdatePassword from './UpdatePassword'
+import DeleteAccount from './DeleteAccount'
+import Search from './Search'
+import { useState } from 'react'
+import { retrieveUser } from '../logic'
+
+function Home(props) {
+    const { token } = props
+
+    const [view, setView] = useState('search')
+    const [name, setName] = useState('name')
+
+    try {
+        retrieveUser(token)
+            .then(user => setName(user.name))
+            .catch(error => alert(error.message))
+    } catch(error) {
+        alert(error.message)
+    }
+
+    return <div className="home">
+        <div className="home__header">
+            <a className="home__home-link" href=""><img className="home__logo" src="images/demo-logo.png" alt="" /></a>
+            <h1 className="home__user">{name}</h1>
+            <a className="home__profile-link" href="">Profile</a>
+            <button className="home__logout-button">Logout</button>
+        </div>
+
+        {view === 'search' && <Search />}
+
+        {view === 'profile' && <Profile />}
+
+        {view === 'update-password' && <UpdatePassword />}
+
+        {view === 'delete-account' && <DeleteAccount />}
+    </div>
+}
+
+export default Home
