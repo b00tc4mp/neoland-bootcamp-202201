c496b69852386d9ff7da14c222cf1f5b94670a1a
Judith Domènech <judith@MacBook-Pro-de-Judith.local>
2022-02-11 09:42:51 +0100

    add zen-app.1

---
+++ staff/judith-domenech/web/zen-app.1/src/data/manu.js
@@ -0,0 +1,14 @@
+const manu = [
+    'Esta chulo el Pirulo',
+    'Ahi ta ahi ta ahi ta',
+    'Ui ui ui ui ui ui',
+    'Se va a enterar, se va a enterar',
+    'La he liado Pardo',
+    "Se fue a la arbolada",
+    'Esta chulo el Pirulo',
+    'Esta chulo el Pirulo',
+    'Esta chulo el Pirulo',
+    'Se va a enterar, se va a enterar',
+    'Se va a enterar, se va a enterar',
+    'Se va a enterar, se va a enterar',
+]
