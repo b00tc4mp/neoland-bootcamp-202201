3c46f9b64aef34bedcd569991b11144408a772e0
Ventu Rodríguez <brodriguezserrano54@gmail.com>
2022-02-11 18:38:07 +0100

    add css example

--- staff/ventura-rodriguez/web/baby-app/src/App.js
+++ staff/ventura-rodriguez/web/baby-app/src/App.js
@@ -3,22 +3,24 @@ import './App.css';
 import { useState } from 'react'
 
 import Register from './components/Register'
+import Landing from './components/Landing'
 
 
 function App() {
 
-	const [view, setView] = useState('register')
+	const [view, setView] = useState('landing')
 
 
 	const showLogin = () => setView('login')
+	const showRegister = () => setView('register')
 
 
-	return <>
-		{view === 'landing' && <h1>Hello landing</h1>}
+	return <div className="app">
+		{view === 'landing' && <Landing onLogin={showLogin} onRegister={showRegister}/>}
 		{view === 'login' && <h1>Hello login</h1>}
 		{view === 'register' && <Register onLogin={showLogin}/>}
 		{view === 'home' && <h1>Hello home</h1>}
-	</>
+	</div>
 
 }
 
