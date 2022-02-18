000e67c36fb96bcba6d77ed71e9034ccecdecf2f
manuelbarzi <manuelbarzi@gmail.com>
2022-02-18 09:42:05 +0100

    add orders logics

---
+++ staff/manuel-barzi/web/demo-app/src/logic/place-vehicles-order.js
@@ -0,0 +1,5 @@
+function placeVehiclesOrder(token) {
+    // TODO generate order id
+    // TODO call api to get user cart array
+    // TODO call api to add order with id and date into orders array (and empty cart array)
+}
