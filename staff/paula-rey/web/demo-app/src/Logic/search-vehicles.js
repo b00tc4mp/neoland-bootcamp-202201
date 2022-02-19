c1e8d77d7400f242c11721ff1c155f447f8c718b
Paula Rey <preynava@gmail.com>
2022-02-19 14:40:12 +0100

    add advances in demo-app new version

--- staff/paula-rey/web/demo-app/src/Logic/search-vehicles.js
+++
@@ -1,8 +0,0 @@
-import { validateQuery } from './helpers/validators'
-
-function searchVehicles(query) {
-    return fetch(`https://b00tc4mp.herokuapp.com/api/hotwheels/vehicles?q=${query}`)
-        .then(res => res.json())
-}
-
-export default searchVehicles
