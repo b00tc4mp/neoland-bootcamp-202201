50d55e0ec1b4ee31aaa4d689a3345d991ebb8f0d
Alba Casas <albacasasarzua1b@gmail.com>
2022-02-22 18:35:19 +0100

    solve car-race and gta wip

--- staff/alba-casas/full-stack/grand-theft-auto/index.js
+++ staff/alba-casas/full-stack/grand-theft-auto/index.js
@@ -16,10 +16,21 @@ let intervals = 0;
 const intervalId = setInterval(() => {
   console.clear();
 
-  let steps = randomInteger(0, 5);
+  let steps = randomInteger(0, 1);
   thieve.x += steps;
+
+  steps = randomInteger(0, 5);
+  police.x += steps;
 
   let road = "";
 
-  if (thieve.x < 10) road = " ".repeat(thieve.x) + thieve.icon;
-});
+  if (thieve.x >= 0 && thieve.x <= 10)
+    road = " ".repeat(thieve.x) + thieve.icon;
+  else if (thieve.x >= 10)
+    road =
+      " ".repeat(police.x + 10) +
+      police.icon +
+      " ".repeat(thieve.x - 10) +
+      thieve.icon;
+  console.log(road);
+}, 100);
