7e4d4339dc840f6fa1d4ae28560e0c53da2edf40
Ventu Rodríguez <brodriguezserrano54@gmail.com>
2022-02-23 12:07:12 +0100

    add process-data

--- staff/ventura-rodriguez/full-stack/convert/index.js
+++ staff/ventura-rodriguez/full-stack/convert/index.js
@@ -6,11 +6,35 @@ if (command === 'copy') {
 
     fs.readFile('./input/lorem.txt', 'utf-8')
     .then(data => fs.writeFile('./output/lorem-copy.txt', data))
-    .then(() => console.log('success'))
+    .then(() => console.log('success copy'))
     .catch(console.log)
 
 }
+else if (command === 'process-data') {
 
-// TODO proccess-data to  lorem-process.txt || '0' should be 'o', '5' should be 's', '1' should be 'l'
-// Not use Array.prototype or others prototypes
+    fs.readFile('./input/lorem.txt', 'utf-8')
+    .then(_data => {
+
+        let data = ''
+
+        for(let i = 0; i < _data.length; i++) {
+
+            const character = _data[i]
+
+            if      (character === '0') data += 'o' // data = data + 'o'
+            else if (character === '1') data += 'l'
+            else if (character === '5') data += 's'
+            else                        data += character
+
+        }
+
+        return fs.writeFile('./output/lorem-process-data.txt', data)
+    })
+    .then(() => console.log('success process-data'))
+    .catch(console.log)
+
+}
 else console.log('command not found')
+
+
+// node --inspect-brk . process-data
