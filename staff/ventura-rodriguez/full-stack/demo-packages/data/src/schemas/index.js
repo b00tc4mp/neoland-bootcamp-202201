0780f856acbd520062fcd223f643cab215512e09
Ventu Rodríguez <brodriguezserrano54@gmail.com>
2022-02-24 14:53:29 +0100

    add demo-packages

---
+++ staff/ventura-rodriguez/full-stack/demo-packages/data/src/schemas/index.js
@@ -0,0 +1,17 @@
+const user = require('./user')
+const creditCard = require('./creditCard')
+const property = require('./property')
+const brand = require('./brand')
+const product = require('./product')
+const stock = require('./stock')
+const order = require('./order')
+
+module.exports = {
+    user,
+    creditCard,
+    property,
+    brand,
+    product,
+    stock,
+    order
+}
