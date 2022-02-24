d065194e836e3f14dfdc615da214b14fe9fa07d5
Aleperaza <alejandroperaza286@gmail.com>
2022-02-24 23:26:33 +0000

    updating demo

---
+++ staff/alejandro-peraza/full-stack/logic/src/updateUserPassword.js
@@ -0,0 +1,11 @@
+const { models: { User } } = require('data')
+
+function updateUserPassword(id, { name, email }) {
+    return User.findById(id)
+    .then((user) => {
+      user.name = name || user.name;
+      user.email = email || user.email;
+  
+      return user.save();
+    })
+}
