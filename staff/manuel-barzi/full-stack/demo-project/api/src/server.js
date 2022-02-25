e727e604524c9040d43db25c1cc9874dbb9d0616
manuelbarzi <manuelbarzi@gmail.com>
2022-02-25 13:27:23 +0100

    integrate logic into api

--- staff/manuel-barzi/full-stack/demo-project/api/src/server.js
+++ staff/manuel-barzi/full-stack/demo-project/api/src/server.js
@@ -1,15 +1,44 @@
+const { mongoose: { connect, disconnect }} = require('data')
 const express = require('express')
+const { registerUser } = require('logic')
+const cors = require('cors')
 
-const api = express()
+connect('mongodb://localhost:27017/demo-db')
+    .then(() => console.log('db connected'))
+    .then(() => {
+        const server = express()
 
-api.get('/hello', (req, res) => { // middleware
-    const name = req.query.name
+        server.use(cors())
 
-    //res.set('Content-type', 'text/html')
-    res.set('Content-type', 'application/json')
+        const jsonBodyParser = express.json()
+        
+        const api = express.Router()
 
-    //return res.send(`<h1>Hello, ${name}!</h1>`)
-    res.status(200).json({ hello: name })
-})
+        api.get('/hello', (req, res) => { // middleware
+            //const name = req.query.name
+            const { name, surname } = req.query
+        
+            //res.set('Content-type', 'text/html')
+            //res.set('Content-type', 'application/json')
+        
+            //return res.send(`<h1>Hello, ${name}!</h1>`)
+            //res.status(200).json({ hello: { name, surname } })
+            res.json([name, surname])
+        })
+        
+        api.post('/users', jsonBodyParser, (req, res) => {
+            try {
+                const { body: { name, email, password } } = req
 
-api.listen(8080, () => console.log('server started'))
+                registerUser(name, email, password)
+                    .then(() => res.status(201).send())
+                    .catch(error => res.status(400).json({ error: error.message }))
+            } catch(error) {
+                res.status(400).json({ error: error.message })
+            }
+        })
+
+        server.use('/api', api)
+        
+        server.listen(8080, () => console.log('server started'))
+    })
