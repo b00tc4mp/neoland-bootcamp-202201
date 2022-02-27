00ea11edc8f195b75cb8d0da47b4572e00e6b50d
Paula Rey <preynava@gmail.com>
2022-02-27 19:29:12 +0100

    working in demo-project whit logic package and api

--- staff/paula-rey/full-stack/demo-project/api/src/server.js
+++ staff/paula-rey/full-stack/demo-project/api/src/server.js
@@ -1,7 +1,8 @@
 const { mongoose: { connect, disconnect }} = require('data')
 const express = require('express')
-const { registerUser, retrieveUser } = require('logic')
+const { registerUser, retrieveUser, authenticateUser } = require('logic')
 const cors = require('cors')
+const { user } = require('data/src/Schemas')
 
 connect('mongodb://localhost:27017/demo-db')
     .then(() => console.log('db connected'))
@@ -33,25 +34,25 @@ connect('mongodb://localhost:27017/demo-db')
                 const { body: { name, email, password } } = req
 
                 registerUser(name, email, password)
-                    .then(() => res.status(201).send())
-                    .catch(error => res.status(400).json({ error: error.message }))
+                .then(() => res.status(201).send())
+                .catch(error => res.status(400).json({ error: error.message }))
             } catch(error) {
                 res.status(400).json({ error: error.message })
             }
         })
-
-
-        // api.post('/users', jsonBodyParser, (req, res) => {
-        //     try {
-        //         const { body: { email, password } } = req
-
-        //         authenticateUser( email, password)
-        //             .then(() => res.status(200).send({ id }))
-        //             .catch(error => res.status(401).json({ error: error.message }))
-        //     } catch(error) {
-        //         res.status(401).json({ error: error.message })
-        //     }
-        // })
+        
+        
+        api.post('/users/auth', jsonBodyParser, (req, res) => {
+            try {
+                const { body: { email, password } } = req
+                
+                authenticateUser(email, password)
+                .then(() => res.status(200).send(user.id))
+                .catch(error => res.status(401).json({ error: error.message }))
+            } catch(error) {
+                res.status(401).json({ error: error.message })
+            }
+        })
 
 
         // api.get('/users', (req, res) => { 
--- staff/paula-rey/full-stack/demo-project/logic/src/retrieveUser.js
+++ staff/paula-rey/full-stack/demo-project/logic/src/retrieveUser.js
@@ -2,12 +2,11 @@ const { models: { User } } = require('data')
 
 function retrieveUser(id) {
     return User.findById(id)
-        .then(user => {
-            if (!id) throw new Error('id invalid')
-
-            return user
-        })
-
+        .then((user) => user)
 }
+
+// function retrieveUser(id) {
+//     return User.findById(id).then((user) => user);
+//   }
 
 module.exports = retrieveUser
