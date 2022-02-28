9958b447eaef43c4c93f34148ea46130068439cc
Judith Domènech <judith@MacBook-Pro-de-Judith.local>
2022-02-28 13:31:58 +0100

    add logic

--- staff/judith-domenech/full-stack/demo-project/logic/src/retrieveUser.js
+++ staff/judith-domenech/full-stack/demo-project/logic/src/retrieveUser.js
@@ -1,8 +1,10 @@
 const { models: { User } } = require('data')
+const {validators : {vali}}
 
 function deleteUser(id) {
     return User.findById(id)
         .then(user => user.deleteOne({ _id: id}))
+
 }
 
 module.exports = deleteUser
---
+++ staff/judith-domenech/full-stack/demo-project/logic/src/updateUserPassword.js
@@ -0,0 +1,12 @@
+const { models: { User } } = require("data")
+
+function updateUserPassword( id, { currPassword, newPassword }) {
+  return User.findById(id)
+  .then(async (user) => {
+    if (user.password === currPassword) {
+      return User.updateOne({ _id: id }, { password: newPassword })
+    } else throw new Error("Wrong credentials")
+  });
+}
+
+module.exports = updateUserPassword
