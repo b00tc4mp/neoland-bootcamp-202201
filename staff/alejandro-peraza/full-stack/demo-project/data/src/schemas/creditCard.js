e1248f266e121b1e04ae56ea4b94b8bf5d76d4c4
Aleperaza <alejandroperaza286@gmail.com>
2022-02-28 00:11:53 +0000

    adding demo

---
+++ staff/alejandro-peraza/full-stack/demo-project/data/src/schemas/creditCard.js
@@ -0,0 +1,23 @@
+const { Schema } = require('mongoose')
+
+
+const creditCard = new Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    
+    
+    number: {
+        type: String,
+        required: true
+    },
+
+    expiration: {
+        type: Date,
+        required: true
+    },
+
+})
+
+module.exports = creditCard
