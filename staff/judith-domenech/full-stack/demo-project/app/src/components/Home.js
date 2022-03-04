ca2378afac717e72ab3096939faf0dfa886bd4bf
Judith Domènech <judith@MacBook-Pro-de-Judith.local>
2022-03-04 10:45:33 +0100

    refactor logic-app

--- staff/judith-domenech/full-stack/demo-project/app/src/components/Home.js
+++ staff/judith-domenech/full-stack/demo-project/app/src/components/Home.js
@@ -1,8 +1,12 @@
+import { Profile, UpdatePassword, DeleteAccount} from './index'
 import { retrieveUser } from '../logic'
 import { useEffect, useState } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 
 function Home() {
     const [name, setName] = useState()
+    const navigate = useNavigate()
+    const [view, setView] = useState()
 
     useEffect(() => {
         try {
@@ -14,8 +18,27 @@ function Home() {
         }
     })
 
-    return <div>
+    const showProfile = () => navigate('profile')
+
+    const goToProfile = event => {
+        event.preventDefault()
+        showProfile()
+    }
+
+    const showUpdatePassword=() => navigate('profile/update-password')
+    const showDeleteAccount = () => navigate('profile/delete-account')
+    
+
+    return <div className='home'>
+        <nav className='home__header'></nav>
+        <a href='' onClick={goToProfile}>Profile</a>
         <h1>welcome home, {name}!</h1>
+
+        <Routes>
+            <Route path='profile' element={<Profile  onUpdatePassword={showUpdatePassword} onDeleteAccount={showDeleteAccount} />}></Route>
+            <Route path='profile/update-password' element={<UpdatePassword onBack={showProfile} />} ></Route>
+            <Route path='profile/delete-account' element={<DeleteAccount onBack={showProfile} />}></Route>
+        </Routes>
     </div>
 }
 
--- staff/judith-domenech/full-stack/demo-project/app/src/components/index.js
+++ staff/judith-domenech/full-stack/demo-project/app/src/components/index.js
@@ -1,9 +1,15 @@
 import Register from './Register'
 import Login from './Login'
 import Home from './Home'
+import Profile  from './Profile'
+import UpdatePassword from './UpdatePassword'
+import DeleteAccount from './DeleteAccount'
 
 export {
     Register,
     Login,
-    Home
+    Home,
+    Profile,
+    UpdatePassword,
+    DeleteAccount
 }
