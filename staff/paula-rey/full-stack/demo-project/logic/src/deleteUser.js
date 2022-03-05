310524237fb5d11313f1a6e683bdde14e41cf870
Paula Rey <preynava@gmail.com>
2022-03-05 10:00:28 +0100

    making changes in package and index

--- staff/paula-rey/full-stack/demo-project/logic/src/deleteUser.js
+++ staff/paula-rey/full-stack/demo-project/logic/src/deleteUser.js
@@ -8,7 +8,7 @@ function deleteUser(userId, password) {
 
     return Note.deleteMany({ user: userId })
         .then(() => {
-            return User.deleteOne({ id: userId, password })
+            return User.deleteOne({ _id: userId, password })
                 .then(result => {
                     if (result.deletedCount === 0) throw new Error(`wrong user ${userId} or password`)
                 })
