1527b1b8ad9cde3649cb2c145d3298228bd5e1a2
Judith Domènech <judith@MBP-de-Judith.home>
2022-03-07 18:49:53 +0100

    add api handlers and index

---
+++ staff/judith-domenech/full-stack/final-project/api/src/handlers/retrieveUser.js
@@ -0,0 +1,15 @@
+const { verifyTokenAndGetUserId } = require('../helpers')
+const { retrieveUser } = require('logic')
+
+module.exports = (req, res) => {
+    try {
+        const userId = verifyTokenAndGetUserId(req)
+
+        retrieveUser(userId)
+            .then(user => res.json(user))
+            .catch(error => res.status(400).json({ error: error.message }))
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
+
---
+++ staff/judith-domenech/full-stack/final-project/api/src/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const { mongoose: { connect, disconnect } } = require('data')
+const express = require('express')
+const {
+    registerUser,
+    authenticateUser,
+    retrieveUser,
+    updateUser,
+    updatePassword,
+    deleteUser,
+   
+} = require('./handlers')
+
+const cors = require('cors')
+const { env: { PORT, MONGODB_URL }} = process
+
+connect(MONGODB_URL)
+    .then(() => console.log('db connected'))
+    .then(() => {
+        const server = express()
+
+        server.use(cors())
+
+        const jsonBodyParser = express.json()
+
+        const api = express.Router()
+
+        api.post('/users', jsonBodyParser, registerUser)
+        api.post('/users/auth', jsonBodyParser, authenticateUser)
+        api.get('/users', retrieveUser)
+        api.patch('/users', jsonBodyParser, updateUser)
+        api.patch('/users/change-password', jsonBodyParser, updatePassword)
+        api.delete('/users', jsonBodyParser, deleteUser)
+
+        server.use('/api', api)
+
+        server.listen(PORT, () => console.log('server started'))
+    })
