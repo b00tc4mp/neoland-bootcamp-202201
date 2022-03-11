057ed1521c23bc6bc4f54ad07a1789b6e43acdfd
Paula Rey <preynava@gmail.com>
2022-03-11 19:07:34 +0100

    working in handlers in api

--- staff/paula-rey/full-stack/final-project/api/src/handlers/index.js
+++ staff/paula-rey/full-stack/final-project/api/src/handlers/index.js
@@ -4,6 +4,12 @@ const retrieveUser = require('./retrieveUser')
 const updateUser = require('./updateUser')
 const updateUserPassword = require('./updateUserPassword')
 const deleteUser = require('./deleteUser')
+const createLocation = require('./createLocation')
+const retrieveLocation = require('./retrieveLocation')
+const updateLocation = require('./updateLocation')
+const listUserLocations = require('./listUserLocations')
+const deleteLocation = require('.deleteLocation')
+const searchLocations = require('./searchLocations')
 
 
 module.exports = {
@@ -13,5 +19,11 @@ module.exports = {
     updateUser,
     updateUserPassword,
     deleteUser,
+    createLocation,
+    retrieveLocation,
+    updateLocation,
+    listUserLocations,
+    deleteLocation,
+    searchLocations
    
 }
--- staff/paula-rey/full-stack/final-project/api/src/index.js
+++ staff/paula-rey/full-stack/final-project/api/src/index.js
@@ -8,6 +8,13 @@ const { registerUser,
         updateUser,
         updateUserPassword,
         deleteUser,
+        createLocation,
+        retrieveLocation,
+        updateLocation,
+        listUserLocations,
+        deleteLocation,
+        searchLocations
+
      } = require('./handlers')
 
 const cors = require('cors')
@@ -26,12 +33,26 @@ connect(MONGODB_URL)
         const api = express.Router()
 
 
-        api.post('/users', jsonBodyParser, registerUser) //ojo! no ejecutar funcion: handlerRegisterUser()
+        api.post('/users', jsonBodyParser, registerUser) 
         api.post('/users/auth', jsonBodyParser, authenticateUser)
         api.get('/users', retrieveUser)
         api.patch('/users', jsonBodyParser, updateUser)
         api.patch('/users/change-password', jsonBodyParser, updateUserPassword)
         api.delete('/users', jsonBodyParser, deleteUser)
+
+        api.post('/locations', jsonBodyParser, createLocation)
+        api.get('/user/:userId/locations', jsonBodyParser, listUserLocations)
+        api.patch('/locations/:locationId', jsonBodyParser, updateLocation)
+        api.get('/locations/:locationId', jsonBodyParser, retrieveLocation)
+        api.delete('/locations/:locationId', jsonBodyParser, deleteLocation)
+        api.get('/locations/?q=', )
+        
+        api.post('/comments', jsonBodyParser, createComment)
+        api.get('/comments/:commentId', jsonBodyParser, retrieveComment)
+        api.get('/' listLocationsComments)
+        api.delete('/', deleteComment)
+
+
 
         server.use('/api', api)
 
