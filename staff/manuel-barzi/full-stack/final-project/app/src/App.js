6984b526a84b1a8d7e19b8377ed4eeedfcd7ecd2
manuelbarzi <manuelbarzi@gmail.com>
2022-03-18 13:03:45 +0100

    add sass styles

--- staff/manuel-barzi/full-stack/final-project/app/src/App.js
+++ staff/manuel-barzi/full-stack/final-project/app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.sass';
+import Hello from './components/Hello'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Hello to="World" />
     </div>
   );
 }
---
+++ staff/manuel-barzi/full-stack/final-project/app/src/components/Hello.js
@@ -0,0 +1,7 @@
+import './Hello.sass'
+
+function Hello({to}) {
+    return <h1 class="hello">Hello, {to}!</h1>
+}
+
+export default Hello
